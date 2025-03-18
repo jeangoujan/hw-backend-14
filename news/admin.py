ed6772d14c6f7db836af8f976c822e89265881a0
jeangoujan <damir.zhanguzhanov@list.ru>
2025-03-18 17:42:20 +0500

    added admin + django-debug-toolbar

--- news/admin.py
+++ news/admin.py
@@ -13,10 +13,11 @@ admin.site.register(Comment)
 
 @admin.register(News)
 class AdminNews(admin.ModelAdmin):
-    fields = ("title", "content", "created_at", "display_has_comments")
+    list_display = ("title", "content", "created_at", "display_has_comments")
+    fields = ("title", "content", "created_at")
     readonly_fields = ("created_at",)
     inlines = [CommentInline]
 
-    @admin.display(boolean=True, description="Есть комментарии")
+    @admin.display(boolean=True, description="Has Comments")
     def display_has_comments(self, obj):
         return obj.has_comments()
